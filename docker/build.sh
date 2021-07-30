5a4951cfbfcab58ad6db4100c6bd14fc93dc7541
spectralcodex <fr3man.stephen@gmail.com>
2021-07-30 14:37:18 +0000

    docker contents updated with likes, comment mc

--- docker/build.sh
+++ docker/build.sh
@@ -7,3 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t "ms-saysay/api-gateway" $DIR/../api-gateway
 docker build -t "ms-saysay/story-microservice" $DIR/../story-microservice
 docker build -t "ms-saysay/administration-microservice" $DIR/../administration-microservice
+docker build -t "ms-saysay/comment-microservice" $DIR/../comment-microservice
+docker build -t "ms-saysay/likes-microservice" $DIR/../likes-microservice
